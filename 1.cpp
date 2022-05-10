42fc399ab1a0167b4d507c3a8d5227cb25a8358e
codewithparvesh <94281338+codewithparvesh@users.noreply.github.com>
2022-05-10 15:27:36 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=5;
+    
+    // loogic for pattern
+    for(int n=0;n<x;n++){
+        for(int p=0;p<x;p++){
+            if(p<=n){
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+}
